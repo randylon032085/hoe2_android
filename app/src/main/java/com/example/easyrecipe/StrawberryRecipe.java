16dcb8f1775e5cc82b181dd4c371b8d8da8de020
Randylon Torda <randylontorda@Randylons-Laptop.local>
2025-01-23 22:55:01 -0700

    first commit

---
+++ app/src/main/java/com/example/easyrecipe/StrawberryRecipe.java
@@ -0,0 +1,39 @@
+package com.example.easyrecipe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class StrawberryRecipe extends AppCompatActivity {
+
+    Button buttonBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_strawberry_recipe);
+
+        buttonBack = findViewById(R.id.btnBack);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void ViewBack(View view){
+
+
+        startActivity(new Intent(StrawberryRecipe.this,MainActivity.class));
+    }
+
+}
